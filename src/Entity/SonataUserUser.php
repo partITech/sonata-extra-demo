dffc7b7f1da44942863adff49ee019d945f7ba66
tbourdin <tbourdin@partitech.com>
2024-10-22 12:26:09 +0200

    Correct User entity with SonataExtra contract UserInterface

--- src/Entity/SonataUserUser.php
+++ src/Entity/SonataUserUser.php
@@ -4,10 +4,11 @@ namespace App\Entity;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Sonata\UserBundle\Entity\BaseUser3;
+use Partitech\SonataExtra\Contract\UserInterface;
 
 #[ORM\Entity]
 #[ORM\Table(name: 'user__user')]
-class SonataUserUser extends BaseUser3
+class SonataUserUser extends BaseUser3 implements UserInterface
 {
     #[ORM\Id]
     #[ORM\Column(type: Types::INTEGER)]
